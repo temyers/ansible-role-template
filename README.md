be5d68d91dac5e5479b440ea518aee36e22e683c
Tim Myerscough <tim.myerscough@magentys.io>
2016-06-22 09:43:12 +0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ansible-role-template
 Template for creating Ansible Galaxy Roles
+
+This creates an Ansible Galaxy role, with support for testing using Serverspec.
+
+# Pre-Requisites
+
+* Ansible must be installed on the development machine.
+* Ruby and serverspec must be installed on the development machine.
+* Git is installed
+
+# To create a new role:
+
+1. Create a new Git repository for your role
+2. Clone this repository: `git clone git@github.com:temyers/ansible-role-template.git`
+2. CD to the project directory
+3. Run `init.sh` to create initialise your repository
---
+++ init.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+#http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+TEMPLATE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "This script will initialise your new Ansible Galaxy role from this template"
+echo "CTRL+C to abort."
+echo "Before starting, you need to create the new git repository that will contain your new role"
+echo -n "Enter the url to clone your new repository (e.g. 'git@github.com:temyers/ansible-role-template.git'): "
+read REPOSITORY_URL
+
+echo -n "Enter name of new Ansible Galaxy role: "
+read ANSIBLE_ROLE
+
+echo `dirname $TEMPLATE_DIR`/$ANSIBLE_ROLE
+if [[ -e "`dirname $TEMPLATE_DIR`/$ANSIBLE_ROLE"  ]]
+then
+    echo "'$ANSIBLE_ROLE' already exists in the parent directory"
+    exit 1
+fi
+
+
+echo "The current git configuration will be deleted and replaced to point to: $REPOSITORY_URL"
+echo "Current git configuration:"
+echo "###########################"
+git config -l --local
+echo "###########################"
+echo ""
+echo "This directory shall be renamed to '$ANSIBLE_ROLE' as part of the re-configuration"
+echo -n "Initialise Role? [Y/N]: "
+read OPTION
+
+if [ "Y" == "$OPTION" || "y" == "$OPTION" ]
+then
+    echo -n "Removing git configuration for ansible-role-template..."
+    rm -rf $TEMPLATE_DIR/.git
+    echo "OK"
+
+    echo -n "Configuring for git repostiory: $REPOSITORY_URL..."
+    cd $TEMPLATE_DIR
+    git init
+    git remote add origin $REPOSITORY_URL
+    echo "OK"
+
+    echo -n "Reconfiguring for Ansible Galaxy..."
+    cd $TEMPLATE_DIR
+    CURRENT_DIR = basename `pwd`
+    cd ..
+    mv $CURRENT_DIR $ANSIBLE_ROLE
+    NEW_FULL_PATH = `pwd`/$ANSIBLE_ROLE
+    ansible-galaxy init $ANSIBLE_ROLE --force
+    echo "OK"
+
+    echo  "Initialising Serverspec..."
+    cd $ANSIBLE_ROLE
+    #Replace the Travis CI build
+    mv .travis.yml.template .travis.yml
+    serverspec-init
+    echo "OK"
+
+else
+    echo "Aborted..."
+fi
+
+
